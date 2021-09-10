716e83fef9026bb581a61818d102db61f2a366f8
rachelhs <rachelsmith400@hotmail.co.uk>
2021-09-10 15:28:49 +0100

    boundary value on up and down

--- run-scales.py
+++ run-scales.py
@@ -1,24 +1,31 @@
-# Sample code to demonstrate Encoder class.  Prints the value every 5 seconds, and also whenever it changes.
-
 import time
 import RPi.GPIO as GPIO
 from encoder import Encoder
 
+# keeps track of whether a person is standing on the scales or not
+onToggle = False
+boundaryVal = 80
+
 def valueChanged(value):
     print(value)
-    if (value == 40):
+    if (value == boundaryVal and onToggle == False):
         print("PERSON STEPPING ON")
+        onToggle = True
+    elif (value == boundaryVal and onToggle == True):
+        print("PERSON STEPPING OFF")
+        onToggle = False
 
 GPIO.setmode(GPIO.BCM)
 
 # 17 is the white wire, 18 is the green wire
 e1 = Encoder(18, 17, valueChanged)
 
+# run code on loop
 try:
     while True:
         time.sleep(5)
-        #print("Value is {}".format(e1.getValue()))
 except Exception:
         pass
 
+# reset pins before exit
 GPIO.cleanup()

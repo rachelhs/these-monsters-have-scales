ba8923c8cb52ba88d4fbdfa878277666f7a5979e
rachelhs <rachelsmith400@hotmail.co.uk>
2021-09-10 14:51:50 +0100

    value in while loop

--- run-scales.py
+++ run-scales.py
@@ -17,7 +17,7 @@ e1 = Encoder(18, 17, valueChanged)
 
 try:
     while True:
-        time.sleep(5)
+        #time.sleep(5)
         if (pos == 40):
             print("PERSON STEPPING ON")
         #print("Value is {}".format(e1.getValue()))

25316ae5dcff3d6813f59f32099f87a6c2a9285d
rachelhs <rachelsmith400@hotmail.co.uk>
2021-09-10 14:50:05 +0100

    value in while loop

--- run-scales.py
+++ run-scales.py
@@ -4,7 +4,10 @@ import time
 import RPi.GPIO as GPIO
 from encoder import Encoder
 
+pos = 0
+
 def valueChanged(value):
+    pos = value
     print("* New value: {}".format(value))
 
 GPIO.setmode(GPIO.BCM)
@@ -15,7 +18,9 @@ e1 = Encoder(18, 17, valueChanged)
 try:
     while True:
         time.sleep(5)
-        print("Value is {}".format(e1.getValue()))
+        if (pos == 40):
+            print("PERSON STEPPING ON")
+        #print("Value is {}".format(e1.getValue()))
 except Exception:
     pass
 

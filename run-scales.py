88a683d71f49c60a636708febe5ac6a4e19b1add
rachelhs <rachelsmith400@hotmail.co.uk>
2021-09-10 14:30:47 +0100

    better encoder library

--- run-scales.py
+++ run-scales.py
@@ -1,47 +1,22 @@
-from RPi import GPIO
-from time import sleep
+# Sample code to demonstrate Encoder class.  Prints the value every 5 seconds, and also whenever it changes.
 
-# white wire
-clk = 17
-# green wire
-dt = 18
+import time
+import RPi.GPIO as GPIO
+from encoder import Encoder
+
+def valueChanged(value):
+    print("* New value: {}".format(value))
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-counter = 0
-clkLastState = GPIO.input(clk)
+# 17 is the white wire, 18 is the green wire
+e1 = Encoder(17, 18, valueChanged)
 
-# above this value is person getting on scales, below is person getting off
-boundaryVal = 10
-# keeps track of whether person is stood on scales or not
-onToggle = False
+# try:
+#     while True:
+#         time.sleep(5)
+#         print("Value is {}".format(e1.getValue()))
+# except Exception:
+#     pass
 
-try:
-        while True:
-                # prevent negative values
-                clkState = GPIO.input(clk)
-                dtState = GPIO.input(dt)
-                if clkState != clkLastState:
-                        if dtState != clkState:
-                                counter += 1
-                        else:
-                                # don't let counter go below 0
-                                if (counter < 0):
-                                    counter = 0
-                                else:
-                                    counter -= 1
-                        if (counter == boundaryVal and onToggle == False):
-                            onToggle = True
-                            print("Person standing on")
-                        elif (counter == boundaryVal and onToggle == True):
-                            onToggle = False
-                            print("Person getting off")
-                            # set counter back to 0
-                            counter = 0
-                        print(counter)
-                clkLastState = clkState
-                sleep(0.01)
-finally:
-        GPIO.cleanup()
+GPIO.cleanup()

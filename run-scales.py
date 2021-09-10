4775af9a2bb83c7564b6610f2144ed7df0743c75
rachelhs <rachelsmith400@hotmail.co.uk>
2021-09-10 15:02:13 +0100

    try callback on value changed

--- run-scales.py
+++ run-scales.py
@@ -5,8 +5,8 @@ import RPi.GPIO as GPIO
 from encoder import Encoder
 
 def valueChanged(value):
-    global pos = value
-    print(pos)
+    global pos
+    pos = value
     #print("* New value: {}".format(value))
 
 GPIO.setmode(GPIO.BCM)
@@ -16,7 +16,7 @@ e1 = Encoder(18, 17, valueChanged)
 
 try:
     while True:
-        #print(pos)
+        print(pos)
         time.sleep(5)
         if (pos == 40):
             print("PERSON STEPPING ON")

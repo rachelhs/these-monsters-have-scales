4c1827defaa4d4c62f5d6130014976de55dfe3f0
rachelhs <rachelsmith400@hotmail.co.uk>
2021-09-10 12:22:53 +0100

    add boundary val

--- run-scales.py
+++ run-scales.py
@@ -13,6 +13,9 @@ GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 counter = 0
 clkLastState = GPIO.input(clk)
 
+# above this value is person getting on scales, below is person getting off
+boundaryVal = 80
+
 try:
         while True:
                 clkState = GPIO.input(clk)
@@ -22,6 +25,8 @@ try:
                                 counter += 1
                         else:
                                 counter -= 1
+                        if (counter == boundaryVal):
+                            print("ACTION")
                         print(counter)
                 clkLastState = clkState
                 sleep(0.01)

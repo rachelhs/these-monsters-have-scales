6d8229d14cab841197b3e39d3dd82bd827fd9733
rachelhs <rachelsmith400@hotmail.co.uk>
2021-09-10 12:38:35 +0100

    try adding boundary vals and no negative

--- run-scales.py
+++ run-scales.py
@@ -15,21 +15,31 @@ clkLastState = GPIO.input(clk)
 
 # above this value is person getting on scales, below is person getting off
 boundaryVal = 10
+# keeps track of whether person is stood on scales or not
+onToggle = False
 
 try:
         while True:
                 # prevent negative values
-                if (counter < 0):
-                    counter = 0
                 clkState = GPIO.input(clk)
                 dtState = GPIO.input(dt)
                 if clkState != clkLastState:
                         if dtState != clkState:
                                 counter += 1
                         else:
-                                counter -= 1
-                        if (counter == boundaryVal):
-                            print("ACTION")
+                                # don't let counter go below 0
+                                if (counter < 0):
+                                    counter = 0
+                                else:
+                                    counter -= 1
+                        if (counter == boundaryVal and onToggle == False):
+                            onToggle = True
+                            print("Person standing on")
+                        else if (counter == boundaryVal and onToggle == True):
+                            onToggle = False
+                            print("Person getting off")
+                            # set counter back to 0
+                            counter = 0
                         print(counter)
                 clkLastState = clkState
                 sleep(0.01)

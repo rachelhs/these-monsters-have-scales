2043d5924b954ee7cb1d521235a4538752e1bd6a
rachelhs <rachelsmith400@hotmail.co.uk>
2021-09-15 12:38:47 +0100

    stop everything after track has finished as fallback

--- run-scales.py
+++ run-scales.py
@@ -55,7 +55,7 @@ def valueChanged(value):
         # track that 1 more person has stood on the scales
         tracker = tracker + 1
     # fallback in case scales value doesn't go back down properly
-    if (onToggle):
+    elif (onToggle):
         print(channel.get_busy())
 
 # 17 is the white wire, 18 is the green wire

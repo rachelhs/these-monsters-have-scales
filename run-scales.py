b0154534a7399d4ebc736f3353c50c5492f7ca14
rachelhs <rachelsmith400@hotmail.co.uk>
2021-09-15 12:06:57 +0100

    stop everything after track has finished as fallback

--- run-scales.py
+++ run-scales.py
@@ -54,7 +54,7 @@ def valueChanged(value):
         # track that 1 more person has stood on the scales
         tracker = tracker + 1
     # fallback in case scales value doesn't go back down properly
-    elif (onToggle == True):
+    # elif (onToggle == True):
 
         # for event in pygame.event.get():
         #     if event.type == TRACK_END:

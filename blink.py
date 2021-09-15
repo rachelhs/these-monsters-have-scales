e4414ca3ca8c5264dd4a057f246b597a287f1fe3
rachelhs <rachelsmith400@hotmail.co.uk>
2021-09-15 17:09:23 +0100

    add led when programme running

--- blink.py
+++ blink.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO    # Import Raspberry Pi GPIO library
 from time import sleep     # Import the sleep function from the time module
 
-GPIO.setmode(GPIO.BOARD)   # Use physical pin numbering
+GPIO.setmode(GPIO.BCM)   # Use physical pin numbering
 GPIO.setup(26, GPIO.OUT, initial=GPIO.LOW)   # Set pin 8 to be an output pin and set initial value to low (off)
 
 while True: # Run forever

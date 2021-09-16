5612dd2d788fbba4430c46b868d18fe4261dbbd2
rachelhs <rachelsmith400@hotmail.co.uk>
2021-09-16 12:46:09 +0100

    multiple relay module test

--- relay-test.py
+++ relay-test.py
@@ -8,7 +8,7 @@ GPIO.setup(7, GPIO.OUT, initial=GPIO.LOW)
 
 while True: # Run forever
     GPIO.output(8, GPIO.HIGH) # Turn on
-    GPIO.output(7, GPIO.LOW) # Turn on
+    GPIO.output(7, GPIO.HIGH) # Turn on
     time.sleep(2)                  # Sleep for 2 seconds
     GPIO.output(8, GPIO.LOW)  # Turn off
     GPIO.output(7, GPIO.LOW) # Turn on

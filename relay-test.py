76d579ab577953784d1bdd0fe635c3b53c78aa3f
rachelhs <rachelsmith400@hotmail.co.uk>
2021-09-16 12:29:17 +0100

    multiple relay module test

---
+++ relay-test.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(7, GPIO.OUT, initial=GPIO.LOW)
+
+while True: # Run forever
+    GPIO.output(8, GPIO.HIGH) # Turn on
+    GPIO.output(7, GPIO.LOW) # Turn on
+    sleep(2)                  # Sleep for 2 seconds
+    GPIO.output(8, GPIO.LOW)  # Turn off
+    GPIO.output(7, GPIO.LOW) # Turn on
+    sleep(2)                  # Sleep for 2 seconds

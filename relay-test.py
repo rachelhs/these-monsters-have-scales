1d74ccad1d6585fb4f37c53645750827d3cb1e16
rachelhs <rachelsmith400@hotmail.co.uk>
2021-09-17 11:31:15 +0100

    add party horn

--- relay-test.py
+++ relay-test.py
@@ -8,11 +8,11 @@ GPIO.setup(7, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(5, GPIO.OUT, initial=GPIO.LOW)
 
 while True: # Run forever
-    GPIO.output(8, GPIO.HIGH) # Turn on
+    # GPIO.output(8, GPIO.HIGH) # Turn on
     GPIO.output(7, GPIO.HIGH) # Turn on
-    GPIO.output(5, GPIO.HIGH) # Turn on
+    # GPIO.output(5, GPIO.HIGH) # Turn on
     time.sleep(2)                  # Sleep for 2 seconds
-    GPIO.output(8, GPIO.LOW)  # Turn off
+    # GPIO.output(8, GPIO.LOW)  # Turn off
     GPIO.output(7, GPIO.LOW) # Turn on
-    GPIO.output(5, GPIO.LOW) # Turn on
+    # GPIO.output(5, GPIO.LOW) # Turn on
     time.sleep(2)                  # Sleep for 2 seconds

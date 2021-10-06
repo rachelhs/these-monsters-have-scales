bf271794c8d0c84588298fd15c253adc8906d99a
rachelhs <rachelsmith400@hotmail.co.uk>
2021-10-06 15:10:08 +0100

    remove Bach from sounds

--- test.py
+++ test.py
@@ -1,1 +1,5 @@
-print("hello")
+import os
+
+directory = r'/Users/rachel/Documents/These_Monsters_Have_Scales/tmhs-raspberrypi/sounds'
+for filename in os.listdir(directory):
+        print(os.path.join(directory, filename))
